d1f40e226115ef84bd0b327466f5104ebb0e70b5
Rajinikanth008 <rajinikanth1993@gmail.com>
2017-05-20 15:37:05 +0530

    Adds arrays custom directives

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 var app = angular.module("myApp",[]);
 
 app.controller("myController",["$scope",function($scope){
-	$scope.message = "myController"
+	$scope.message = "This message from controller"
+	$scope.companyName = "Techmind pvt ltd"
+     
+     $scope.arrayOfUsers = [
+            {
+   name:"papa srinivas",
+   address:"Telangana,hyderanad"
+	},
+	{
+   name:"swan ",
+   address:"delhi"
+	}
+     ]
+	
+	$scope.changeCompanyName = function(){
+		$scope.companyName = "Techmind solutions"
+	}
+
 }])
 
 app.directive("myCustomerDetailsDirective",function(){
 	return {
-		restrict:'EAC',
-		template:"<h1>this complex directive</h1>",
-		replace:true
+		restrict:'EACM',
+		templateUrl:  'customerDetails.html',
+                  
 
+		replace:true,
+        scope:{
+        	companyName : '@',
+        	userDetails : '=',
+        	change      : '&'
+        }
 	}
+
 })
